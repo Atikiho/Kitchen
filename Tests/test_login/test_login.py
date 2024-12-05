1231c0bde11ea1fc5cee02add1baed8ebb9275d2
user <nikitaskrypets@gmail.com>
2024-12-05 10:30:29 +0200

    upgrade tests structure

--- Tests/test_login/test_login.py
+++ Tests/test_login/test_login.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 
 from Kitchen_app.models import Cook, DishType
 
@@ -52,25 +52,3 @@ class TestLogin(TestCase):
                 username="Test123",
                 password="Test")
         )
-
-
-class TestSearch(TestCase):
-    def test_search_dish(self):
-
-        data = {
-            "name": "H",
-            "price_min": 1,
-            "price_max": 20,
-            "dish_type": 1,
-        }
-        response = self.client.get(
-            reverse("dish-list"),
-            data,
-            follow=True
-        )
-
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue("name" in response.request.get("QUERY_STRING"))
-        self.assertTrue("price_min" in response.request.get("QUERY_STRING"))
-        self.assertTrue("price_max" in response.request.get("QUERY_STRING"))
-        self.assertTrue("dish_type" in response.request.get("QUERY_STRING"))
